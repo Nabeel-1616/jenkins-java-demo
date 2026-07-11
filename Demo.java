84bd2b15abae333a1a37299e637ac29b087fbb18
Nabeel-1616 <awsnabeel01@gmail.com>
2026-07-11 11:42:17 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Jenkins automatic build is working!");
     }
 }
