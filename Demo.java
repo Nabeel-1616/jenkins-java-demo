b0674d979e27ec67a646a80e6c996fef0ae025f2
Nabeel-1616 <awsnabeel01@gmail.com>
2026-07-14 20:42:34 +0530

    Change output message in Demo class

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Jenkins automatic build is working!");
+        System.out.println("Jenkins!");
     }
 }
