245ef94de7ab9a56a644006e69cdc68b9aee546a
Nabeel-1616 <awsnabeel01@gmail.com>
2026-07-17 19:16:21 +0530

    Fix print statement in Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Jenkins 12345678910 !");
-        System.out.println("now this is for showing that the webhook is working properly"
+        System.out.println("now this is for showing that the webhook is working properly!");
     }
 }
